f8007d5316270aa87102d685bccea2b6210cb6d3
Dale Wells <dw_7672@pm.me>
2020-05-09 09:25:02 +0100

    Moved driver into a fixture Changed teardown methods

--- features/environment.py
+++ features/environment.py
@@ -10,16 +10,27 @@ SECTIONS = {  # links of the navigation bar
 }
 
 
-def before_feature(context, feature):
+@fixture
+def driver(context):
     context.driver = webdriver.Chrome()
-    context.driver.get(HOMEPAGE)
+    yield
+    context.driver.quit()
+
+
+def before_all(context):
+    use_fixture(driver, context)
     context.registration_page = RegistrationPage(context.driver)
 
 
+def before_scenario(context, scenario):
+    context.driver.get(HOMEPAGE)
+
+
+def after_scenario(context, scenario):
+    context.driver.delete_all_cookies()
+
+
+# Common steps exist inside of environment.py
 @given("the user is on the {section} page")
 def navigate_to(context, section):
     context.driver.find_element_by_link_text(SECTIONS[section]).click()
-
-
-def after_feature(context, feature):
-    context.driver.quit()
